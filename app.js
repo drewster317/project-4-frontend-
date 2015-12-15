62e50aff9430c0544fa5a7f4c78d6e7e20d25832
Drewster317 <drewster317@gmail.com>
2015-12-15 09:57:25 -0500

    CREATED a new state for posts and a controller

--- app.js
+++ app.js
@@ -1,18 +1,25 @@
 var app = angular.module('dreadit', ['ui.router']);
 app.config([
-'$stateProvider',
-'$urlRouterProvider',
-function($stateProvider, $urlRouterProvider) {
+  '$stateProvider',
+  '$urlRouterProvider',
+  function($stateProvider, $urlRouterProvider) {
 
-  $stateProvider
+    $stateProvider
     .state('home', {
       url: '/home',
       templateUrl: '/home.html',
       controller: 'MainCtrl'
+    })
+
+    .state('posts', {
+      url: '/posts/{id}',
+      templateUrl: '/posts.html',
+      controller: 'PostsCtrl'
     });
 
-  $urlRouterProvider.otherwise('home');
-}]);
+    $urlRouterProvider.otherwise('home');
+
+  }])
 
 
 app.factory('posts', [function(){
@@ -36,12 +43,20 @@ app.controller('MainCtrl', [
       $scope.title = '';
       $scope.link = '';
 
-      }
-
-      $scope.incrementUpvotes = function(post) {
-        post.upvotes += 1;
     }
 
+    $scope.incrementUpvotes = function(post) {
+      post.upvotes += 1;
+    }
+
+  }])
+app.controller('PostsCtrl', [
+  '$scope',
+  '$stateParams',
+  'posts',
+  function($scope, $stateParams, posts){
+
   }]);
+
 
 
